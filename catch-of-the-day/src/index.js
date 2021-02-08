e089f1d7e6988ae4693e4446a35faa5970e4d8ac
ionita-tech <lenuta.ionita@yahoo.com>
2021-02-08 14:56:33 +0200

    Revert "creating firs component"
    This reverts commit c590aa942d3cc7ff4979702a57946fa426879900.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
-
-
-render(<StorePicker/>, document.querySelector('#main'));
+// let's go!
